d06780adc0962e9012d3c05ad59b8e5ba556eaab
Andrew Waters <drew@Andrews-MacBook-Pro.local>
2020-01-16 10:46:31 -0600

    initial commit

---
+++ env/lib/python3.7/site-packages/apispec/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""Contains main apispec classes: `APISpec` and `BasePlugin`"""
+
+from .core import APISpec
+from .plugin import BasePlugin
+
+__version__ = "2.0.2"
+__all__ = ["APISpec", "BasePlugin"]
