e55e7a658b6fda6898033fd561f78e25e35ffb71
Andrew Waters <andrew.e.waters@gmail.com>
2020-04-13 13:03:49 -0500

    fixed another text mishap in the instruction routes

--- wrst/routes/reading_routes.py
+++ wrst/routes/reading_routes.py
@@ -19,7 +19,7 @@ def display_reading_instructions():
     header = "First, you are going to read a brief section from an introductory Biology textbook"
     content_items = Markup(
         """<p>During this portion of the study, please read the text that is linked at the bottom of this page. Don't worry about memorizing all of the information. Just try get a general familiarity with the topic to the best of your ability.<br><br>
-        Please spend at least 10 minutes reading the text. We have provided a timer on this page to help you keep track. <br><br>
+        Please spend at least 5 minutes reading the text. We have provided a timer on this page to help you keep track. <br><br>
         When the timer expires, you can click on the 'Next' button to move to the next step of the activity.<br></p>
         """
     )
